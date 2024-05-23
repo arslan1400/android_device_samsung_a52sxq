56996ca155747ea746d941e79d0a16dd4e05af6c
Arslan <132056853+arslan1400@users.noreply.github.com>
2024-05-23 10:38:24 +0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -28,6 +28,10 @@ $(call inherit-product, vendor/samsung/a52sxq/a52sxq-vendor.mk)
 PRODUCT_PACKAGES += \
     init.a52sxq.rc
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RISING_CHIPSET="Snapdragon 778G" \
+    RISING_MAINTAINER="Arslan"
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
